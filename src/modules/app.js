0033295ac4fd08a0805607836421e7926a021910
Bart Bieszczad <bbart318@gmail.com>
2023-02-10 01:41:53 -0600

    Build expanded task view and rename functions

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,6 +1,7 @@
 export default function appController() {
   const tasksWrapper = document.querySelector('.t-wrapper');
   const formWrapper = document.querySelector('.f-wrapper');
+  const expandWrapper = document.querySelector('.e-wrapper');
 
   const showForm = () => {
     formWrapper.style.animation = 'ease-out formRight 0.1s';
@@ -9,50 +10,100 @@ export default function appController() {
       formWrapper.style.animation = '';
     }, 100);
   };
-  const animateFormOut = () => {
+  const hideForm = () => {
     formWrapper.style.animation = 'ease-out formRight reverse 0.1s';
     setTimeout(() => {
       formWrapper.style.display = 'none';
       formWrapper.style.animation = '';
     }, 100);
   };
-  const showTasks = () => {
+  const showTasksRight = () => {
     tasksWrapper.style.display = 'flex';
     tasksWrapper.style.animation = 'ease-out taskRight reverse 0.1s';
     setTimeout(() => {
       tasksWrapper.style.animation = '';
     }, 100);
   };
+  const showTasksLeft = () => {
+    tasksWrapper.style.display = 'flex';
+    tasksWrapper.style.animation = 'ease-out formRight 0.1s';
+    setTimeout(() => {
+      tasksWrapper.style.animation = '';
+    }, 100);
+  };
 
-  const animateTasksOut = () => {
+  const hideTasksRight = () => {
     tasksWrapper.style.animation = 'ease-out taskRight 0.1s';
     setTimeout(() => {
       tasksWrapper.style.display = 'none';
       tasksWrapper.style.animation = '';
     }, 100);
   };
+  const hideTasksLeft = () => {
+    tasksWrapper.style.animation = 'ease-out formRight reverse 0.1s';
+    setTimeout(() => {
+      tasksWrapper.style.display = 'none';
+      tasksWrapper.style.animation = '';
+    }, 100);
+  };
 
+  const expandTask = () => {
+    expandWrapper.style.display = 'flex';
+    expandWrapper.style.animation = 'ease-out taskRight reverse 0.1s';
+    setTimeout(() => {
+      expandWrapper.style.animation = '';
+    }, 100);
+  };
+  const closeTask = () => {
+    expandWrapper.style.animation = 'ease-out taskRight 0.1s';
+    setTimeout(() => {
+      expandWrapper.style.display = 'none';
+      expandWrapper.style.animation = '';
+    }, 100);
+  };
   const renderForm = () => {
-    animateTasksOut();
+    hideTasksRight();
     setTimeout(() => {
       showForm();
     }, 100);
   };
   const renderTasks = (e) => {
     e.preventDefault();
-    animateFormOut();
+    if (formWrapper.style.display === 'flex') {
+      hideForm();
+      setTimeout(() => {
+        showTasksRight();
+      }, 100);
+      return;
+    }
+    if (expandWrapper.style.display === 'flex') {
+      closeTask();
+      setTimeout(() => {
+        showTasksLeft();
+      }, 100);
+    }
+  };
+  const renderExpandedTask = () => {
+    hideTasksLeft();
     setTimeout(() => {
-      showTasks();
+      expandTask();
     }, 100);
   };
 
+  const checkmark = document.querySelector('.fa-regular');
+  const closeBtn = document.querySelector('.close-btn');
   const backBtn = document.querySelector('.back-btn');
   const addBtn = document.querySelector('.add-btn');
+  const task = document.querySelector('.task');
   backBtn.addEventListener('click', renderTasks);
+  closeBtn.addEventListener('click', renderTasks);
   addBtn.addEventListener('click', renderForm);
+  task.addEventListener('click', renderExpandedTask);
 
-  const checkmark = document.querySelector('.fa-regular');
-  checkmark.addEventListener('click', () => {
+  const toggleCheckmark = () => {
     checkmark.classList.toggle('fa-solid');
-  });
+    checkmark.classList.toggle('fa-circle');
+    checkmark.classList.toggle('fa-circle-check');
+  };
+  checkmark.addEventListener('click', toggleCheckmark);
 }
