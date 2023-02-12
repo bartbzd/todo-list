40413e354eb3902cfcd0f419c4f5b904a82fd622
Bart Bieszczad <bbart318@gmail.com>
2023-02-12 03:39:39 -0600

    Add data-id attribute to created tasks

--- src/modules/views/taskView.js
+++ src/modules/views/taskView.js
@@ -1,12 +1,16 @@
+import { tasks } from '../models/taskModel';
+
 export default function createTask(task) {
   const taskWrapper = document.createElement('div');
   const checkmark = document.createElement('div');
   const title = document.createElement('p');
+
   const actions = document.createElement('div');
   const edit = document.createElement('i');
   const trash = document.createElement('i');
   const star = document.createElement('i');
 
+  taskWrapper.setAttribute('data-id', tasks.indexOf(task));
   taskWrapper.classList.add('task');
   checkmark.classList.add('checkmark');
   title.classList.add('task-title');
@@ -16,7 +20,6 @@ export default function createTask(task) {
   edit.textContent = 'edit';
   trash.classList.add('options', 'material-symbols-rounded');
   trash.textContent = 'delete';
-
   star.classList.add('fa-solid', 'fa-star');
 
   document.querySelector('.tasks').append(taskWrapper);
