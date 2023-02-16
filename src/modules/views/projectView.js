14ce8dce9d3cae5301657d5dc036371bcd5c5a0c
Bart Bieszczad <bbart318@gmail.com>
2023-02-16 03:08:15 -0600

    Build project creation view

---
+++ src/modules/views/projectView.js
@@ -0,0 +1,25 @@
+export default function createProject(project) {
+  const div = () => document.createElement('div');
+  const text = document.createElement('p');
+  const icon = () => document.createElement('i');
+
+  const wrapper = div();
+  wrapper.className = 'project';
+  text.className = 'project-name';
+  text.textContent = project.name;
+  const folder = icon();
+  folder.className = 'material-symbols-rounded';
+  folder.textContent = 'folder';
+  const group = div();
+  group.className = 'project-btn-grp';
+  const edit = icon();
+  edit.classList.add('options', 'edit', 'material-symbols-rounded');
+  edit.textContent = 'edit';
+  const trash = icon();
+  trash.classList.add('options', 'material-symbols-rounded');
+  trash.textContent = 'delete';
+
+  document.querySelector('.project-grp').appendChild(wrapper);
+  wrapper.append(folder, text, group);
+  group.append(edit, trash);
+}
