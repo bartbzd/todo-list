9cc96d758a5769ca95dbc85c14a10b43752c4d02
Bart Bieszczad <bbart318@gmail.com>
2023-03-01 00:57:55 -0600

    Clean up code

--- src/modules/models/taskModel.js
+++ src/modules/models/taskModel.js
@@ -18,11 +18,4 @@ export default class Task {
   getIsStarred() {
     return this.isStarred;
   }
-  //   editTask(title, note, project, date) {
-  //     this.title = title;
-  //     this.note = note;
-  //     this.project = project;
-  //     this.date = date;
-  //     this.isStarred = false;
-  //   }
 }
