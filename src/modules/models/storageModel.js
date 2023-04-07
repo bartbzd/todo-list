dd5886c638854fbab8bf823e1b91bc99a30b6268
Bart Bieszczad <bbart318@gmail.com>
2023-04-06 22:17:12 -0500

    Build and work on storageModel

--- src/modules/models/storageModel.js
+++ src/modules/models/storageModel.js
@@ -2,10 +2,13 @@ import Task from './taskModel';
 import Project from './projectModel';
 
 let projects = [];
+let allTasksList = new Project('All');
 
 export default function storage() {
+  let all = allTasksList.getTasks();
+  console.log(allTasksList.getTasks());
   let data = {
-    projects,
+    projects: [],
     tasks: projects.flatMap((project) => project.getTasks()),
   };
 
@@ -13,10 +16,10 @@ export default function storage() {
     localStorage.setItem('data', JSON.stringify(data));
   }
 
-  function getProjects() {
+  function getData() {
     const storedData = localStorage.getItem('data');
     data = JSON.parse(storedData);
-    // console.log(data);
+    console.log(data);
     // console.log(data.projects[0]);
     const tempProjects = data.projects.map((project) => {
       const tempTasks = project.tasks.map(
@@ -37,15 +40,58 @@ export default function storage() {
       tasks: tempProjects.flatMap((project) => project.getTasks()),
     };
     projects = tempProjects;
-    // console.log(data);
-    // console.log(projects);
+    // allTasksList = new Project('All');
+    const allTasks = all.map(
+      (task) =>
+        new Task(
+          task.title,
+          task.note,
+          task.project,
+          task.date,
+          task.isStarred,
+          task.isComplete
+        )
+    );
+    console.log(allTasks);
+    allTasksList = new Project('All', allTasks);
   }
+  //   function saveAllTasks() {
+  //     localStorage.setItem('allTasks', JSON.stringify(allTasksList));
+  //   }
+  //   function getAllTasks() {
+  //     const storedAllTasks = localStorage.getItem('allTasks');
+  //     console.log(storedAllTasks);
+  //     if (storedAllTasks) {
+  //       all = JSON.parse(storedAllTasks);
+  //     } else {
+  //       all = new Project('All');
+  //     }
+  //     console.log(all);
+  //     const tempTasks = all.map(
+  //       (task) =>
+  //         new Task(
+  //           task.title,
+  //           task.note,
+  //           task.project,
+  //           task.date,
+  //           task.isStarred,
+  //           task.isComplete
+  //         )
+  //     );
 
+  //     all = tempTasks.flatMap((task) => task.getTasks());
+  //     allTasksList = tempTasks;
+  //     // projects = tempProjects;
+  //   }
   return {
-    getProjects,
+    getData,
     saveData,
     projects,
+    // saveAllTasks,
+    // getAllTasks,
+    allTasksList,
   };
 }
 
 export { projects };
+export { allTasksList };
