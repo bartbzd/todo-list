aa44272afead12f4b20d5976d660ca6dc47a3e8b
Bart Bieszczad <bbart318@gmail.com>
2023-04-09 02:09:35 -0500

    Refactor and clean up code

--- src/modules/models/storageModel.js
+++ src/modules/models/storageModel.js
@@ -3,13 +3,8 @@ import Project from './projectModel';
 
 let projects = [];
 let allTasksList = new Project('All');
-function setAllTasksList(tasks) {
-  allTasksList = new Project('All', tasks);
-}
+
 export default function storage() {
-  //   let all = allTasksList.getTasks();
-  //   console.log(allTasksList.getTasks());
-  console.log(allTasksList);
   let data = {
     projects,
     tasks: projects.flatMap((project) => project.getTasks()),
@@ -18,17 +13,14 @@ export default function storage() {
 
   function saveData() {
     localStorage.setItem('data', JSON.stringify(data));
-    // localStorage.setItem('allTasksData', JSON.stringify(all));
-    console.log('Im here');
   }
 
   function getData() {
     const storedData = localStorage.getItem('data');
     data = JSON.parse(storedData);
-    console.log(data);
-    // console.log(data.projects[0]);
-    const tempProjects = data.projects.map((project) => {
-      const tempTasks = project.tasks.map(
+
+    const storedProjects = data.projects.map((project) => {
+      const storedTasks = project.tasks.map(
         (task) =>
           new Task(
             task.title,
@@ -39,11 +31,9 @@ export default function storage() {
             task.isComplete
           )
       );
-      return new Project(project.name, tempTasks);
+      return new Project(project.name, storedTasks);
     });
 
-    // allTasksList = new Project('All');
-    console.log(data.all);
     const allTasks = data.all.map(
       (task) =>
         new Task(
@@ -55,42 +45,17 @@ export default function storage() {
           task.isComplete
         )
     );
-    // console.log(allTasks);
 
-    data.projects = tempProjects;
-    data.tasks = tempProjects.flatMap((project) => project.getTasks());
+    data.projects = storedProjects;
+    data.tasks = storedProjects.flatMap((project) => project.getTasks());
     data.all = allTasks;
-    projects = tempProjects;
+    projects = storedProjects;
     allTasksList = new Project('All', allTasks);
   }
-  //   function saveAllTasks() {
-  //     localStorage.setItem('allTasks', JSON.stringify(allTasksList));
-  //   }
-  //   function getAllTasks() {
-  //     const storedAllTasks = localStorage.getItem('allTasks');
-  //     console.log(storedAllTasks);
-  //     if (storedAllTasks) {
-  //       all = JSON.parse(storedAllTasks);
-  //     } else {
-  //       all = new Project('All');
-  //     }
-  //     console.log(all);
-  //     const tempTasks = all.map(
-  //       (task) =>
-  //         new Task(
-  //           task.title,
-  //           task.note,
-  //           task.project,
-  //           task.date,
-  //           task.isStarred,
-  //           task.isComplete
-  //         )
-  //     );
 
-  //     all = tempTasks.flatMap((task) => task.getTasks());
-  //     allTasksList = tempTasks;
-  //     // projects = tempProjects;
-  //   }
+  function setAllTasksList(tasks) {
+    allTasksList = new Project('All', tasks);
+  }
 
   return {
     getData,
@@ -102,4 +67,3 @@ export default function storage() {
 }
 
 export { projects, allTasksList };
-// export { allTasksList };
